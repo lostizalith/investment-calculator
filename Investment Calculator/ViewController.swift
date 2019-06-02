4ce6e063f48e3e5225f9f12ba0c34fe671243dd7
Mikita Kryvitski <solairerove@gmail.com>
2019-06-02 15:16:21 +0300

    feature: initiate brand new project

---
+++ Investment Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Investment Calculator
+//
+//  Created by Nikita Krivitski on 6/2/19.
+//  Copyright © 2019 Nikita Krivitski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
